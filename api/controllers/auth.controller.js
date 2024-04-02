bf3957c777724e3f770c8eefbce649676aea9288
drilya989 <matylda.glazik@gmail.com>
2024-04-02 02:00:15 +0200

    createError

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import createError from "../utils/createError.js";
 
 export const register = async (req, res, next) => {
     try {
@@ -15,25 +17,39 @@ export const register = async (req, res, next) => {
       next(err);
     }
   };
-export const login = async (req,res) =>{
+export const login = async (req,res, next) =>{
 
     try{
         const user = await User.findOne({email: req.body.email})
-        if(!user) return res.status(404).send("User not found")
+        
+        if(!user) return next(createError(404,"User not found"));
 
         const isCorrect = bcrypt.compareSync(req.body.password, user.password)
-        if(!isCorrect) return res.status(400).send("Wrong password or username")
+        if(!isCorrect) return next(createError(400,"Wrong password or username"));
+        
+        const token = jwt.sign(
+        {
+          id: user._id,
+        },
+        process.env.JWT_KEY
+        );
+
         const {password, ...info} = user._doc
-        res.status(200).send(info)
+        res.cookie("accessToken",token, {
+          httpOnly:true,
+        }).status(200).send(info)
 
     }
-    catch{
-        res.status(500).send("something went wrong")
+    catch (err){
+       next(err);
 
 
     }
 
 }
 export const logout = async (req,res) =>{
-
+  res.clearCookie("accessToken", {
+    sameSite:"none",
+    secure: true,
+  }).status(200).send("User has been loged out");
 }
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,14 @@
-export const fn = (req,res) =>{
+import User from "../models/user.model.js";
+import createError from "../utils/createError.js";
+
+
+
+export const deleteUser = async (req,res, next) =>{
+    const user = await User.findById(req.params.id)
     
-}
+        if(req.userId !== user._id.toString()){
+           return next(createError(403,"You can only delete your account"));
+        }
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).send("deleted");
+};
